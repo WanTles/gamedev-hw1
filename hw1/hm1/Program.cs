8775e436c656a5189a34f03dbabe8b8ec26cc06a
WanTles <mohammadashknani953@gmail.com>
2022-03-10 18:11:17 +0300

    non

---
+++ hw1/hm1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hero = 100;
+            int health = 100;
+            int damaged(int downHP)
+            {
+                return health - downHP;
+            }
+            int playerHP = damaged(20);
+            Console.WriteLine("Damaged Health : " + playerHP);
+
+            int boost(int boostHP)
+            {
+                return playerHP + boostHP;
+            }
+            int bsHP = boost(10);
+            Console.WriteLine("Boosted Health : " + bsHP);
+
+            int[] HealthArray = { playerHP, bsHP };
+            {
+                Console.WriteLine(HealthArray[1]);
+            };
+
+            if (playerHP > bsHP)
+            {
+                Console.WriteLine(playerHP + " is more than " + bsHP);
+            }
+            else if (playerHP < bsHP)
+            {
+                Console.WriteLine(playerHP + " is less than " + bsHP);
+            }
+            else
+            {
+                Console.WriteLine("Neither is more than the another");
+            }
+
+
+        }
+    }
+}
